352e9aae55d3b14e742e452fccc3fd66196aa77c
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-14 13:05:56 +0100

    Create CameraController

---
+++ Assets/scripts/camera/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	[HideInInspector]
+	public Vector3 startPos;
+	[HideInInspector]
+	public bool isFollowing;
+	[HideInInspector]
+	public Transform birdToFollow;
+
+	private float minX = 0, maxX = 20;
+
+	private void Awake () {
+		startPos = transform.position;
+	}
+
+	private void Update () {
+		if (isFollowing) {
+			if (birdToFollow != null) {
+				Vector3 birdPos = birdToFollow.position;
+				float x = Mathf.Clamp (birdPos.x, minX, maxX);
+				transform.position = new Vector3 (x, startPos.y, startPos.z);
+			} else {
+				isFollowing = false;
+			}
+		}
+	}
+}
