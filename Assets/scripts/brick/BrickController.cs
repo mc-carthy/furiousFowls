fe52df76d63ef8852029d37ad40518e206a60a16
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-14 12:45:47 +0100

    Add BrickController

---
+++ Assets/scripts/brick/BrickController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BrickController : MonoBehaviour {
+
+	public float health = 70f;
+
+	private AudioSource source;
+	private float damageMultiplier = 10f;
+
+	private void Awake () {
+		source = GetComponent<AudioSource> ();
+	}
+
+	private void OnCollisionEnter2D (Collision2D col) {
+		if (col.gameObject.GetComponent<Rigidbody2D> () != null) {
+			float projectileVelocity = col.gameObject.GetComponent<Rigidbody2D> ().velocity.magnitude;
+			float damage = projectileVelocity * damageMultiplier;
+			if (damage > 15f) {
+				source.Play ();
+			}
+			health -= damage;
+
+			if (health < 0) {
+				Destroy (gameObject);
+			}
+		}
+	}
+}
