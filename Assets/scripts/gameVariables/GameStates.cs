5ea4a2e724596badc11a13ecf39bc5a8d85b3920
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-14 12:52:11 +0100

    Add GameVariables & GameStates classes

---
+++ Assets/scripts/gameVariables/GameStates.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum SlingshotState {
+	Idle,
+	UserPulling,
+	BirdFlying
+}
+
+public enum GameState {
+	Start,
+	BirdMovingToSlingshot,
+	Playing,
+	Won,
+	Lost
+}
+
+public enum BirdState {
+	BeforeThrown,
+	Thrown
+}
---
+++ Assets/scripts/gameVariables/GameVariables.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameVariables {
+
+	public static float minVelocity = 0.09f;
+	public static float birdColliderRadiusNormal = 0.5f;
+	public static float birdColliderRadiusLarge = 0.65f;
+}
