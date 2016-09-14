a28af4b488664007713753afc3a943d2cdd97a20
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-09-14 16:12:59 +0100

    Allow player to move camera

--- Assets/scripts/camera/CameraController.cs
+++ Assets/scripts/camera/CameraController.cs
@@ -10,7 +10,14 @@ public class CameraController : MonoBehaviour {
 	[HideInInspector]
 	public Transform birdToFollow;
 
+	public SlingshotController slingshot;
+
 	private float minX = 0, maxX = 20;
+	private float minY = 0, maxY = 2.7f;
+
+	private float dragSpeed = 0.01f;
+	private float timeDragStart;
+	private Vector3 previousPosition;
 
 	private void Awake () {
 		startPos = transform.position;
@@ -24,6 +31,31 @@ public class CameraController : MonoBehaviour {
 				transform.position = new Vector3 (x, startPos.y, startPos.z);
 			} else {
 				isFollowing = false;
+			}
+		}
+
+		if (slingshot.state == SlingshotState.Idle && GameController.state == GameState.Playing) {
+			if (Input.GetMouseButtonDown (0)) {
+				timeDragStart = Time.time;
+				dragSpeed = 0f;
+				previousPosition = Input.mousePosition;
+			}
+		// TODO - Remove hardcoded value
+		} else if (Input.GetMouseButton (0) && Time.time - timeDragStart > 0.005f) {
+			Vector3 input = Input.mousePosition;
+			float deltaX = (previousPosition.x - input.x) * dragSpeed;
+			float deltaY = (previousPosition.y - input.y) * dragSpeed;
+
+			float newX = Mathf.Clamp (transform.position.x + deltaX, minX, maxX);
+			float newY = Mathf.Clamp (transform.position.y + deltaY, minY, maxY);
+
+			transform.position = new Vector3 (newX, newY, transform.position.z);
+
+			previousPosition = input;
+
+			// TODO - Remove hardcoded values
+			if (dragSpeed < 0.1f) {
+				dragSpeed += 0.002f;
 			}
 		}
 	}
